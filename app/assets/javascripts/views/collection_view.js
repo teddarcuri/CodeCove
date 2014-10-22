77658e8ca5edf4c81a082968560c3ed6340e0cca
teddarcuri <tarcuri88@gmail.com>
2014-10-22 00:21:38 -0600

    Pretty codewindow styling on collections

--- app/assets/javascripts/views/collection_view.js
+++ app/assets/javascripts/views/collection_view.js
@@ -1,5 +1,19 @@
 // for more details see: http://emberjs.com/guides/views/
 
 Codecove.CollectionView = Ember.View.extend({
-  templateName: 'collection'
+  templateName: 'collection',
+  didInsertElement: function() {
+  	// Loop over all textareas with '.codemirror-textarea' class and apply a special id that Codemirror can use
+	$('.codemirror-textarea').each(function(index) {
+	    $(this).attr('id', 'code-' + index);
+	    CodeMirror.fromTextArea(document.getElementById('code-' + index), {
+	            lineNumbers: true,
+	            tabMode: "indent",
+	            mode: "ruby",
+	            theme: "neo",
+	            //keyMap: "vim"
+	        }
+	    );
+	});
+  }
 });
