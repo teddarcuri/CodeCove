6f4cc86a9ca8f2b122bdf1fa7cf8f2a4f4868d49
teddarcuri <tarcuri88@gmail.com>
2014-11-19 14:26:43 -0700

    link to the code pages by clicking the name in code windows

--- app/assets/javascripts/models/code.js
+++ app/assets/javascripts/models/code.js
@@ -5,7 +5,7 @@ Codecove.Code = DS.Model.extend({
   body: DS.attr('string'),
   description: DS.attr('string'),
   created_at: DS.attr('string'),
-  updated_t: DS.attr('string'),
+  updated_at: DS.attr('string'),
   collections: DS.hasMany('collection'),
   languages: DS.hasMany('language')
 });
