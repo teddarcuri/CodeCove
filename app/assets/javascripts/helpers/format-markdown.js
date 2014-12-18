5351cfe0de4dafd552751606b6e76d10d76b5860
Tedd Arcuri <tedd.arcuri@www.colorado.gov>
2014-12-17 23:50:44 -0700

    Some styling and used conditionals to have markdown in new code template

--- app/assets/javascripts/helpers/format-markdown.js
+++ app/assets/javascripts/helpers/format-markdown.js
@@ -1,5 +1,5 @@
 var showdown = new Showdown.converter();
 
-Ember.Handlebars.helper('format-markdown', function(input) {
+Ember.Handlebars.helper('markdown', function(input) {
   return new Handlebars.SafeString( showdown.makeHtml(input) );
 });
