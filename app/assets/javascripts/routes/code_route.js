09847c9e44540d63f6dd764e2f1ccdc4d3d4ab89
Tedd Arcuri <tedd.arcuri@www.colorado.gov>
2015-05-18 21:33:44 -0600

    adding the alert/delete functionality on the ember side

--- app/assets/javascripts/routes/code_route.js
+++ app/assets/javascripts/routes/code_route.js
@@ -6,8 +6,43 @@ Codecove.CodeRoute = Ember.Route.extend({
 
     actions: {
     	update: function(code) {
-    		code.save();
-    		this.controller.set('isEditing', false);
+
+            var alertBox = $("#alert-box");
+
+            // Update the code
+    	     code.save();
+
+            // Turn off editing mode
+    	     this.controller.set('isEditing', false);
+
+            // Show the alert box
+            alertBox.html("Successfully Updated " + code._data.name);
+            alertBox.addClass("notification").addClass("is-visible");
+
+            // Hide the alert box
+            setTimeout(function () {
+                alertBox.removeClass("is-visible");
+            }, 6000);
+
+    	},
+
+    	delete : function(code) {
+    		var route = this;
+              var alertBox = $("#alert-box");
+
+    		confirm('Are you sure you want to delete "' + code._data.name + '" ?');
+    		code.destroyRecord().then( function() {
+    			route.transitionTo("codes");
+
+                    // Show the alert box
+                    alertBox.html("Succesfully Deleted " + code._data.name);
+                    alertBox.addClass("alert").addClass("is-visible");
+
+                    // Hide the alert box
+                    setTimeout(function () {
+                        alertBox.removeClass("is-visible");
+                    }, 6000);
+    		});
     	}
     }
 });
