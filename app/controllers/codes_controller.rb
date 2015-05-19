1bb44bc8b6a092718cd31551e8d3351af7a89a4c
Tedd Arcuri <tedd.arcuri@www.colorado.gov>
2015-05-18 21:33:11 -0600

    delete code snippet functionality. initial alert boxes implementation.

--- app/controllers/codes_controller.rb
+++ app/controllers/codes_controller.rb
@@ -15,6 +15,10 @@ class CodesController < ApplicationController
         render json: Code.find(params[:id]).tap { |s| s.update_attributes(code) }
     end
 
+     def destroy
+        render json: Code.find(params[:id]).destroy
+    end
+
     private
 	def code
 		params[:code].permit(:name, :description, :body, :created_at, :updated_at, {:collection_ids => []}, {:language_ids => []})
